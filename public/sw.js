e9327e2c85ec59107d5bde775595591e97f835c5
Jandir Clebson <J27SPORTSS@GMAIL.COM>
2026-09-11 14:24:30 -0300

    Atualizar icones J27

---
+++ public/sw.js
@@ -0,0 +1,57 @@
+self.addEventListener("push", (event) => {
+  let data = {
+    title: "Afterpay J27",
+    body: "Você tem uma nova notificação.",
+    url: "/",
+  };
+
+  try {
+    if (event.data) {
+      data = {
+        ...data,
+        ...event.data.json(),
+      };
+    }
+  } catch {
+    if (event.data) {
+      data.body = event.data.text();
+    }
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "/icons/icon-192.svg",
+      badge: "/icons/icon-192.svg",
+      data: {
+        url: data.url || "/",
+      },
+    })
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const url = event.notification.data?.url || "/";
+
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+        includeUncontrolled: true,
+      })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if ("focus" in client) {
+            client.navigate(url);
+            return client.focus();
+          }
+        }
+
+        if (clients.openWindow) {
+          return clients.openWindow(url);
+        }
+      })
+  );
+});
